284740ac4d2b077097c89d8fa5bf9bd6bc327377
Shadow <113185975+ShdwTakashi@users.noreply.github.com>
2024-05-27 18:23:23 -0400

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,1 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+          .setName('ping')
+          .setDescription('Replies with Pong!'),
+
+  run: ({ interaction, client }) => {
+    interaction.reply({ content: `Pong! ${client.ws.ping}ms`, ephemeral: true });
+  },
+  devOnly: true,
+};
 
